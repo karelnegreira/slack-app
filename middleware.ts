9e92db0b6707c82645624bc4a892671dcf04f61a
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-08-30 00:30:41 +1200

    middleware added and signin route created

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { convexAuthNextjsMiddleware, nextjsMiddlewareRedirect } from "@convex-dev/auth/nextjs/server";
+
+import { createRouteMatcher, isAuthenticatedNextjs } from "@convex-dev/auth/nextjs/server";
+
+const isPublicPage = createRouteMatcher(["/signin"]);
+
+export default convexAuthNextjsMiddleware((request) => {
+    if (!isPublicPage && !isAuthenticatedNextjs()) {
+        return nextjsMiddlewareRedirect(request, '/signin')
+    }
+})
+
+export const config = {
+  // The following matcher runs middleware on all routes
+  // except static assets.
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
