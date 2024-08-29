29f52b8cf6c96dcb4e4f943b8bafe429d2c757f2
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-08-29 22:00:37 +1200

    convex schemas generated and conf implemented

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,7 +14,8 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
-import type * as task from "../task.js";
+import type * as auth from "../auth.js";
+import type * as http from "../http.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -25,7 +26,8 @@ import type * as task from "../task.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
-  task: typeof task;
+  auth: typeof auth;
+  http: typeof http;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
