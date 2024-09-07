d2f5d035299d09b2e8bed18b251f9fdfa9bcadf0
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-09-08 00:14:19 +1200

    props aded to use-create-workspace hook

--- src/feature/workspaces/api/use-create-workspaces.ts
+++ src/feature/workspaces/api/use-create-workspaces.ts
@@ -1,12 +1,12 @@
 import { useMutation } from "convex/react";
 
 import { api } from "../../../../convex/_generated/api";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { Id } from "../../../../convex/_generated/dataModel";
 import Error from "next/error";
 
 type RequestType = {name: string};
-type ResponseType = Id<"workspaces">;
+type ResponseType = Id<"workspaces"> | null;
 
 type Options = {
     onSuccess?: (data: ResponseType) => void;
@@ -16,10 +16,26 @@ type Options = {
 };
 
 export const useCreateWorkspace = () => {
-    const mutation = useMutation(api.workspaces.create);
+    const [data, setData] = useState<ResponseType>(null);
+    const [error, setError] = useState<Error | null>(null);
 
+    const [isPending, setIsPending] = useState(false);
+    const [isSuccess, setIsSuccess] = useState(false);
+    const [isError, setIsError] = useState(false);
+    const [isSettled, setIsSettled] = useState(false);
+
+    const mutation = useMutation(api.workspaces.create);
+    
     const mutate = useCallback( async (values: RequestType, options?: Options) => {
         try {
+            setData(null);
+            setError(null);
+            setIsError(false);
+            setIsSettled(false);
+            setIsSuccess(false);
+
+            setIsPending(true);
+            //
             const response = await mutation(values);
             options?.onSuccess?.(response);
             //return response
@@ -31,11 +47,19 @@ export const useCreateWorkspace = () => {
                 throw error;
             }
         } finally {
+            setIsPending(false);
+            setIsSettled(true);
             options?.onSettled?.();
         }
     }, [mutation])
 
     return {
         mutate, 
+        data, 
+        error, 
+        isPending, 
+        isSuccess, 
+        isError, 
+        isSettled
     }
 };
