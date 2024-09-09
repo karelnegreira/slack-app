ace15f8b4cb26d9f31ac838bdeec0d75b331ccce
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-09-09 22:38:52 +1200

    use-get-workspace hook added

---
+++ src/feature/workspaces/api/use-get-workspace.ts
@@ -0,0 +1,14 @@
+import { useQuery } from "convex/react"
+import { api } from "../../../../convex/_generated/api"
+import { Id } from "../../../../convex/_generated/dataModel";
+
+interface UseGetworkspaceProps {
+    id: Id<"workspaces">;
+}
+
+export const useGetworkspaces = ({id}: UseGetworkspaceProps) => {
+    const data = useQuery(api.workspaces.getById, {id});
+    const isLoading = data === undefined;
+
+    return { data, isLoading };
+}
