8c81539f1c46b8ca447156d4e5b732b35e15eef5
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-09-17 02:23:39 +1200

    adding styling to toolbar

--- src/app/workspace/[workspaceId]/toolbar.tsx
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -3,6 +3,10 @@
 const Toolbar = () => {
   return (
     <nav className="bg-[#481349] flex items-center justify-between h-10 p-1.5">
+        <div className="flex-1"/>
+        <div className="min-w-[280px] max-[642px] grow-[2] shrink">
+
+        </div>
         Toolbar
     </nav>
   )
