c2201b6198b8d3930dc3f72c226d27618bd58879
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-09-17 02:14:05 +1200

    styling toolbar component

--- src/app/workspace/[workspaceId]/toolbar.tsx
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -2,7 +2,7 @@
 
 const Toolbar = () => {
   return (
-    <nav className="bg-[#481349]">
+    <nav className="bg-[#481349] flex items-center justify-between h-10 p-1.5">
         Toolbar
     </nav>
   )
