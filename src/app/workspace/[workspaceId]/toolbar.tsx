4df05dda97d27c9d42080728d44c98b6502cd595
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-09-17 02:27:37 +1200

    styling button at toolbar component

--- src/app/workspace/[workspaceId]/toolbar.tsx
+++ src/app/workspace/[workspaceId]/toolbar.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 
 
 const Toolbar = () => {
@@ -5,7 +6,9 @@ const Toolbar = () => {
     <nav className="bg-[#481349] flex items-center justify-between h-10 p-1.5">
         <div className="flex-1"/>
         <div className="min-w-[280px] max-[642px] grow-[2] shrink">
-
+            <Button size="sm" className="bg-accent/25 hover:bg-accent-25 w-full justify-start px-2">
+                
+            </Button>
         </div>
         Toolbar
     </nav>
