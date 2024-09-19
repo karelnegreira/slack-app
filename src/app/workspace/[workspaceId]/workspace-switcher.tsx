75f84406443a54315e897a76fa24b426dc1224ac
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-09-20 02:53:11 +1200

    workspace initial fetched and shown at sidebar button

--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -1,5 +1,9 @@
 
 import { Button } from '@/components/ui/button'
+import { useGetworkspace } from '@/feature/workspaces/api/use-get-workspace'
+import { useGetworkspaces } from '@/feature/workspaces/api/use-get-workspaces'
+import { useWorkspaceId } from '@/hooks/use-workspace-id'
+
 import {
     DropdownMenu, 
     DropdownMenuContent, 
@@ -7,15 +11,33 @@ import {
     DropdownMenuSeparator, 
     DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu'
+import { useCreateWorkspaceModal } from '@/feature/workspaces/store/use-create-workspace-modal'
+import { Loader } from 'lucide-react'
+
 
 export const WorkspacesSwitcher = () => {
+    const workspaceId = useWorkspaceId();
+    const [_open, setOpen] = useCreateWorkspaceModal();
+
+    const {data: workspaces, isLoading: workspacesLoading} = useGetworkspaces();
+    const {data: workspace, isLoading: workspaceLoading } = useGetworkspace({id: workspaceId});
+
+    const filteredWorkspaces = workspaces?.filter( (workspace) => workspace?._id !== workspaceId);
+
+
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger>
                 <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl"> 
-                    A
+                    {workspaceLoading ? (
+                        <Loader className="size-5 animate-spin shrink-0"/>
+                    ) : (workspace?.name.charAt(0).toUpperCase())}
                 </Button>
             </DropdownMenuTrigger>
+            <DropdownMenuContent side="bottom" align="start" className="w-64">
+
+            </DropdownMenuContent>
         </DropdownMenu>
     )
 }
