09bb856f73abb5a318a7f6e09890fec4aca9568a
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-09-20 01:58:09 +1200

    adding style to the button switcher

--- src/app/workspace/[workspaceId]/workspace-switcher.tsx
+++ src/app/workspace/[workspaceId]/workspace-switcher.tsx
@@ -1,10 +1,21 @@
 
-
+import { Button } from '@/components/ui/button'
+import {
+    DropdownMenu, 
+    DropdownMenuContent, 
+    DropdownMenuItem, 
+    DropdownMenuSeparator, 
+    DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 
 export const WorkspacesSwitcher = () => {
     return (
-        <div>
-            Switcher
-        </div>
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Button className="size-9 relative overflow-hidden bg-[#ABABAD] hover:bg-[#ABABAD]/80 text-slate-800 font-semibold text-xl"> 
+                    A
+                </Button>
+            </DropdownMenuTrigger>
+        </DropdownMenu>
     )
 }
