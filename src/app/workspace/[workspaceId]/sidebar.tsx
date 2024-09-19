9dc3b1d86d96c7c9c2da5793f692143488541adc
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-09-20 01:35:09 +1200

    user icon on sidebar added and styled at the bottom

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,9 +1,13 @@
+import { UserButton } from "@/feature/auth/components/user-button"
 
  
  const Sidebar = () => {
    return (
      <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
-        Sidebar
+        <div>A</div>
+        <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
+            <UserButton />
+        </div>
      </aside>
    )
  }
