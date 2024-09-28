5469b104dd387920512c617af9195371d52bbd58
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-09-28 23:12:59 +1200

    adding some styling at sidebar

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,17 +1,14 @@
-"use client";
 
-import { useGetworkspace } from "@/feature/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
-import { useParams } from "next/navigation";
+
 
 
 const WorkspaceIdPage = () => {
-    const workspaceId = useWorkspaceId();
-    const {data} = useGetworkspace({id: workspaceId})
+    
 
     return (
         <div>
-            DATA: {JSON.stringify(data)}
+            Workspace id page
         </div>
     );
 }
