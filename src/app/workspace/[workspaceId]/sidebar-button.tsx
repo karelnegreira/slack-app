68f1070a1a8e60e370cd9bedb0626dea4b0f9c41
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-09-28 23:22:18 +1200

    sidebar-button component was created

---
+++ src/app/workspace/[workspaceId]/sidebar-button.tsx
@@ -0,0 +1,18 @@
+import { LucideIcon } from "lucide-react";
+import { IconType } from "react-icons/lib";
+
+interface SidebarButtonProps {
+    icon: LucideIcon | IconType;
+    label: string;
+    isActive?: boolean;
+}
+
+const SidebarButton = ({icon, label, isActive} : SidebarButtonProps) => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-y-0.5 cursor-pointer group">
+        
+    </div>
+  )
+}
+
+export default SidebarButton;
