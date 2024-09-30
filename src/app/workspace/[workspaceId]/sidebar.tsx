be4d564ecb475d5bdb246a4624b7716143ebd791
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-10-01 03:01:28 +1200

    adding some access control into sidebar

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,17 +1,23 @@
+
+import { usePathname } from "next/navigation"
+
 import { UserButton } from "@/feature/auth/components/user-button"
 import { WorkspacesSwitcher } from "./workspace-switcher"
 import SidebarButton from "./sidebar-button"
 import { Bell, Home, MessageSquare, MoreHorizontal } from "lucide-react"
 
- 
+
  const Sidebar = () => {
+
+  const pathname = usePathname();
+
    return (
      <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
         <WorkspacesSwitcher />
-        <SidebarButton icon={Home} label="Home" isActive />
+        <SidebarButton icon={Home} label="Home" isActive={pathname.includes("/workspace")} />
         <SidebarButton icon={MessageSquare} label="DMs"/>
-        <SidebarButton icon={Bell} label="Home" />
-        <SidebarButton icon={MoreHorizontal} label="Home" />
+        <SidebarButton icon={Bell} label="Activity" />
+        <SidebarButton icon={MoreHorizontal} label="More" />
         <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
             <UserButton />
         </div>
