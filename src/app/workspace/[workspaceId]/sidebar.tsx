ba19366ed39a217d29933e54b697a79d4f40d765
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-10-01 02:53:12 +1200

    sidebar mssg home and more buttons were added

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,13 +1,17 @@
 import { UserButton } from "@/feature/auth/components/user-button"
 import { WorkspacesSwitcher } from "./workspace-switcher"
 import SidebarButton from "./sidebar-button"
+import { Bell, Home, MessageSquare, MoreHorizontal } from "lucide-react"
 
  
  const Sidebar = () => {
    return (
      <aside className="w-[70px] h-full bg-[#481349] flex flex-col gap-y-4 items-center pt-[9px] pb-4">
-        <SidebarButton />
         <WorkspacesSwitcher />
+        <SidebarButton icon={Home} label="Home" isActive />
+        <SidebarButton icon={MessageSquare} label="DMs"/>
+        <SidebarButton icon={Bell} label="Home" />
+        <SidebarButton icon={MoreHorizontal} label="Home" />
         <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
             <UserButton />
         </div>
