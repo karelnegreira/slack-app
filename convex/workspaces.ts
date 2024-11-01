13e47e99bf4cd87563d4faa8c8254229b708fc96
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-11-01 22:25:22 +1200

    worksspaces.ts update with get and getbyid method

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -37,7 +37,30 @@ export const create = mutation({
 export const get = query({
     args: {}, 
     handler: async (ctx) => {
-        return await ctx.db.query("workspaces").collect();
+        const userId = await auth.getUserId(ctx);
+
+        if (!userId) {
+            return [];
+        }
+
+        const members = await ctx.db
+                                .query("members")
+                                .withIndex("by_user_id", (q) => q.eq("userId", userId))
+                                .collect();
+
+        const workspaceIds = members.map((member) => member.workspaceId);
+
+        const workspaces = [];
+
+        for (const workspaceId of workspaceIds) {
+            const workspace = await ctx.db.get(workspaceId);
+
+            if (workspace) {
+                workspaces.push(workspace);
+            }
+        }
+        
+        return workspaces;
     },
 });
 
@@ -48,6 +71,17 @@ export const getById = query({
 
         if (!userId) {
             throw new Error("Unauthorized");
+        }
+
+        const member = await ctx.db
+                            .query("members")
+                            .withIndex("by_workspace_id_user_id", (q) => 
+                            q.eq("workspaceId", args.id).eq("userId", userId), 
+                        )
+                        .unique();
+
+        if(!member) {
+            return null;
         }
 
         return await ctx.db.get(args.id);
