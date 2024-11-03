6de1245fb2256f72f8e9a7afd3a952f5703d607a
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-11-04 01:48:48 +1200

    ResizableGroups RezisablePanel ResiableHandle component formatting added into layout.tsx at workspace

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
+
 import Sidebar from './sidebar';
 import { Toolbar } from './toolbar';
+
 
 
 interface WorkspaceIdLayoutProps {
@@ -14,7 +17,17 @@ const WorkspaceIdLayout = ({children}: WorkspaceIdLayoutProps) => {
         <Toolbar />
         <div className="flex h-[calc(100vh-40px)]">
           <Sidebar />
-          {children}
+          <ResizablePanelGroup direction="horizontal">
+            <ResizablePanel>
+              <div>
+                Channels Sidebar
+              </div>
+            </ResizablePanel>
+            <ResizableHandle withHandle/>
+            <ResizablePanel>
+                {children}
+            </ResizablePanel>
+          </ResizablePanelGroup>
         </div>
     </div>
   )
