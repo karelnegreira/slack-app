b49bd504daa9f921a52aec83bb43a423aa9ad98e
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-11-06 00:39:10 +1200

    WorkspaceHeader component created and being implemented

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -1,8 +1,25 @@
 
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu, 
+    DropdownMenuContent, 
+    DropdownMenuItem, 
+    DropdownMenuSeparator, 
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+
 
 const WorkspaceHeader = () => {
   return (
-    <div>workspace-header</div>
+    <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button>
+                    
+                </Button>
+            </DropdownMenuTrigger>
+        </DropdownMenu>
+    </div>
   )
 }
 
