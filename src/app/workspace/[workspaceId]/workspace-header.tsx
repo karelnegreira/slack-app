9c9e6cbd9b8d77fbfd6219bd043344795c2e1dae
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-11-06 00:48:54 +1200

    Workspace header component at sidebar now show button with title as per workspace

--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -7,15 +7,19 @@ import {
     DropdownMenuSeparator, 
     DropdownMenuTrigger
 } from "@/components/ui/dropdown-menu";
+import { Doc } from "../../../../convex/_generated/dataModel";
 
+interface WorksHeaderProps  {
+    workspace: Doc<"workspaces">;
+}
 
-const WorkspaceHeader = () => {
+const WorkspaceHeader = ({workspace} : WorksHeaderProps) => {
   return (
     <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button>
-                    
+                <Button variant="transparent" className="font-semibold text-lg w-auto p-1.5 overflow-hidden" size="sm">
+                    <span>{workspace?.name}</span>
                 </Button>
             </DropdownMenuTrigger>
         </DropdownMenu>
--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -32,7 +32,7 @@ const WorkspaceSidebar = () => {
 
   return (
     <div className="flex flex-col bg-[#5E2C5F] h-full">
-        <WorkspaceHeader />
+        <WorkspaceHeader workspace={workspace} />
     </div>
   )
 }
