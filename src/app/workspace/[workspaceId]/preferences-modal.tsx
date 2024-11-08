d58dd05a3f138d3761afd57b3beb7de770e241b6
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-11-09 01:28:20 +1200

    preference modal styling

--- src/app/workspace/[workspaceId]/preferences-modal.tsx
+++ src/app/workspace/[workspaceId]/preferences-modal.tsx
@@ -1,9 +1,41 @@
 
+import {
+    Dialog, 
+    DialogContent, 
+    DialogTrigger, 
+    DialogHeader, 
+    DialogTitle, 
+    DialogClose, 
+    DialogFooter, 
+} from '@/components/ui/dialog';
+import { useState } from 'react';
 
-export const PreferencesModal = () => {
+interface PreferenceModalProps {
+    open: boolean;
+    setOpen: (open: boolean) => void;
+    initialValue: string;
+}
+
+export const PreferencesModal = ({open, setOpen, initialValue} : PreferenceModalProps) => {
+    const [value, setValue] = useState(initialValue);
     return (
-        <div>
-            Preferences modal
-        </div>
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent className="p-0 bg-gray-50 overflow-hidden">
+                <DialogHeader className="p-4 border-b bg-white">
+                    <DialogTitle>
+                        {value}
+                    </DialogTitle>
+                </DialogHeader>
+                <div className="px-4 pb-4 flex flex-col gap-y-2">
+                    <div className="px-5 py-4 bg-white rounded-lg border cursor-pointer hover:bg-gray-50">
+                        <div className="flex items-center justify-between">
+                            <p className="text-sm font-semibold">
+                                Workspace name
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </DialogContent>
+        </Dialog>
     )
 }
--- src/app/workspace/[workspaceId]/workspace-header.tsx
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -11,6 +11,8 @@ import {
 } from "@/components/ui/dropdown-menu";
 
 import { Doc } from "../../../../convex/_generated/dataModel";
+import { PreferencesModal } from "./preferences-modal";
+import { useState } from "react";
 
 
 interface WorksHeaderProps  {
@@ -19,71 +21,74 @@ interface WorksHeaderProps  {
 };
 
 const WorkspaceHeader = ({workspace, isAdmin} : WorksHeaderProps) => {
+    const [preferencesOpen, setPreferencesOpen] = useState(false);
+
   return (
     <>
-    <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="transparent" className="font-semibold text-lg w-auto p-1.5 overflow-hidden" size="sm">
-                    <span className="truncate">{workspace.name}</span>
-                    <ChevronDown className="size-4 ml-1 shrink-0"/>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent side="bottom" align="start" className="w-64">
-                <DropdownMenuItem className="cursor-pointer capitalize">
-                    <div className="size-9 
-                                    relative
-                                    overflow-hidden
-                                    bg-[#616061]
-                                    text-white 
-                                    font-semibold 
-                                    text-xl 
-                                    rounded-md
-                                    flex
-                                    items-center 
-                                    justify-center
-                                    mr-2
-                                    ">
-                        {workspace.name.charAt(0).toUpperCase()}
-                    </div>
-                    <div className="flex flex-col items-start">
-                        <p className="font-bold">{workspace.name}</p>
-                        <p className="text-xs text-muted-foreground">Active workspace</p>
-                    </div>
-                </DropdownMenuItem>
-                {isAdmin && (
-                    <>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem 
-                        className="cursor-pointer py-2" 
-                        onClick={() => {}}>
-                            Invite people to {workspace.name}
+    <PreferencesModal open={preferencesOpen} setOpen={setPreferencesOpen} initialValue={workspace.name}/>
+        <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="transparent" className="font-semibold text-lg w-auto p-1.5 overflow-hidden" size="sm">
+                        <span className="truncate">{workspace.name}</span>
+                        <ChevronDown className="size-4 ml-1 shrink-0"/>
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side="bottom" align="start" className="w-64">
+                    <DropdownMenuItem className="cursor-pointer capitalize">
+                        <div className="size-9 
+                                        relative
+                                        overflow-hidden
+                                        bg-[#616061]
+                                        text-white 
+                                        font-semibold 
+                                        text-xl 
+                                        rounded-md
+                                        flex
+                                        items-center 
+                                        justify-center
+                                        mr-2
+                                        ">
+                            {workspace.name.charAt(0).toUpperCase()}
+                        </div>
+                        <div className="flex flex-col items-start">
+                            <p className="font-bold">{workspace.name}</p>
+                            <p className="text-xs text-muted-foreground">Active workspace</p>
+                        </div>
                     </DropdownMenuItem>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem 
-                        className="cursor-pointer py-2" 
-                        onClick={() => {}}>
-                            Preferences
-                    </DropdownMenuItem>
-                    </>
-                )
-                }
-                
-            </DropdownMenuContent>
-        </DropdownMenu>
-        <div className="flex items-center gap-0.5">
-            <Hint label="Filter" side="bottom" align="center">
-                <Button variant="transparent" size="iconSm">
-                    <ListFilter className="size-4"/>
-                </Button>
-            </Hint>
-               <Hint label="new message" side="bottom" align="center">
+                    {isAdmin && (
+                        <>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem 
+                            className="cursor-pointer py-2" 
+                            onClick={() => {}}>
+                                Invite people to {workspace.name}
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem 
+                            className="cursor-pointer py-2" 
+                            onClick={() => {setPreferencesOpen(true)}}>
+                                Preferences
+                        </DropdownMenuItem>
+                        </>
+                    )
+                    }
+                    
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <div className="flex items-center gap-0.5">
+                <Hint label="Filter" side="bottom" align="center">
                     <Button variant="transparent" size="iconSm">
-                        <SquarePen className="size-4"/>
+                        <ListFilter className="size-4"/>
                     </Button>
                 </Hint>
+                <Hint label="new message" side="bottom" align="center">
+                        <Button variant="transparent" size="iconSm">
+                            <SquarePen className="size-4"/>
+                        </Button>
+                    </Hint>
+            </div>
         </div>
-    </div>
     </>
   )
 }
