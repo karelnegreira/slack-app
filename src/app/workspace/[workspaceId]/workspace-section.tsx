6b9acbde3c7741c1bbc681eae8d6274df7f072c5
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-11-26 21:50:28 +1200

    styling the icon FaCaretDown

--- src/app/workspace/[workspaceId]/workspace-section.tsx
+++ src/app/workspace/[workspaceId]/workspace-section.tsx
@@ -13,7 +13,10 @@ export const WorkspaceSection = ( { children, label, hint, onNew } : WorkspaceSe
     return (
         <div className="flex flex-col mt-3 px-2">
             <div className="flex items-center px-3.5 group">
-                <Button>
+                <Button
+                    variant="transparent"
+                    className="p-0.5 text-sm text-[#f9edffcc] shrink-0 size-6"
+                >
                     < FaCaretDown className="size-4"/>
                 </Button>
             </div>
