4fb5237ca751f03cc697005c4c69b0d535e14893
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-12-08 00:08:39 +1200

    styling dropdown button

--- src/app/workspace/[workspaceId]/workspace-section.tsx
+++ src/app/workspace/[workspaceId]/workspace-section.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { PlusIcon } from "lucide-react";
 import { useToggle } from "react-use";
 import { FaCaretDown } from "react-icons/fa";
+import { cn } from "@/lib/utils";
 
 
 interface WorkspaceSectionProps {
@@ -24,7 +25,9 @@ export const WorkspaceSection = ( { children, label, hint, onNew } : WorkspaceSe
                     className="p-0.5 text-sm text-[#f9edffcc] shrink-0 size-6"
                     onClick={toggle}
                 >
-                    < FaCaretDown className="size-4"/>
+                    < FaCaretDown className={cn("size-4 transition-transform", 
+                        on && "-rotate-90"
+                    )} />
                 </Button>
                 <Button 
                     variant="transparent"
