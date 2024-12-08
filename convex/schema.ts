52483aa8f526a53443f42cf21ba0b3adb4a2a8f2
Karel Negreira Fuentes <karelnegreira@gmail.com>
2024-12-08 22:45:57 +1200

    get function added in members.ts and use-current-memebrs hook declared

--- convex/schema.ts
+++ convex/schema.ts
@@ -3,6 +3,7 @@ import { defineSchema, defineTable } from "convex/server";
 import { authTables } from "@convex-dev/auth/server";
 import { v } from "convex/values";
 
+
 const schema = defineSchema({
     ...authTables, 
     workspaces: defineTable({
