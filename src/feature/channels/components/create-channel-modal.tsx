41e747964b45e31c9cbc4d60bf062d1da7aec277
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-01-10 01:24:29 +1200

    create-channel-modal component created

---
+++ src/feature/channels/components/create-channel-modal.tsx
@@ -0,0 +1,26 @@
+import {
+    Dialog, 
+    DialogContent, 
+    DialogDescription, 
+    DialogHeader, 
+    DialogTitle
+} from '@/components/ui/dialog';
+
+import { useCreateChannelModal } from '../store/use-create-channel-modal';
+
+export const CreateChannelModal = () => {
+    
+    const [open, setOpen] = useCreateChannelModal();
+
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Add a channel
+                    </DialogTitle>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+    )
+}
