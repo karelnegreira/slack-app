3e315983ef3bf83248e809913174c145012023c4
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-01-10 01:31:43 +1200

    import create-channel-modal compoenent at modals

--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { CreateWorkspaceModal } from "@/feature/workspaces/components/create-workspace-modal";
+import { CreateChannelModal } from '@/feature/channels/components/create-channel-modal';
+
 import { useEffect, useState } from "react";
 
 export const Modals = () => {
