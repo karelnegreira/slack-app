a91cd72a5d51fed8881d177e2b82c238bdec5ead
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-05-20 01:32:28 +1200

    bug apparentl fix yet need further debuging

--- convex/channels.ts
+++ convex/channels.ts
@@ -1,14 +1,53 @@
-import { v } from "convex/values";
-import { query } from "./_generated/server";
+
+import {v} from 'convex/values';
+
+import { mutation, query } from "./_generated/server";
 import { auth } from "./auth";
 
+
+export const create = mutation({
+    args: {
+        name: v.string(), 
+        workspaceId: v.id("workspaces")
+    }, 
+    handler: async (ctx, args) => {
+        const userId = await auth.getUserId(ctx);
+
+        if (!userId) {
+            return [];
+        }
+
+
+        const member = await ctx
+            .db
+            .query("members")
+            .withIndex("by_workspace_id_user_id",
+            (q: any) => q.eq("workspaceId", args.workspaceId)
+            .eq("userId", userId)
+        )
+            .unique();
+
+        if (!member || member.role !== 'admin') {
+            throw new Error("Unauthorized");
+        }
+
+        const parsedName = args.name.replace(/\s+/g, "-").toLowerCase();
+        
+        const channelId = await ctx.db.insert("channels", {
+            name: parsedName, 
+            workspaceId: args.workspaceId
+        });
+
+        return channelId;
+    }
+})
 
 
 export const get = query({
     args: {
         workspaceId: v.id("workspaces"), 
     }, 
-    handler: async (ctx, args) => {
+    handler: async (ctx: any, args: any) => {
         const userId = await auth.getUserId(ctx);
 
         if (!userId) {
@@ -19,7 +58,7 @@ export const get = query({
             .db
             .query("members")
             .withIndex("by_workspace_id_user_id",
-            (q) => q.eq("workspaceId", args.workspaceId)
+            (q: any) => q.eq("workspaceId", args.workspaceId)
             .eq("userId", userId)
         )
             .unique();
@@ -31,7 +70,7 @@ export const get = query({
         const channels = await ctx.db
                 .query("channels")
                 .withIndex("by_workspace_id", 
-                (q) => q.eq("workspaceId", args.workspaceId))
+                (q: any) => q.eq("workspaceId", args.workspaceId))
                 .collect();
 
         return channels;
