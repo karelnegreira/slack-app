58c7b3c6b151f9f7972035cf39f3e709eb626b92
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-05-27 15:57:34 +1200

    use-create-channel hook updated

--- src/feature/channels/api/use-create-channel.ts
+++ src/feature/channels/api/use-create-channel.ts
@@ -5,8 +5,8 @@ import { useCallback, useMemo, useState } from "react";
 import { Id } from "../../../../convex/_generated/dataModel";
 import Error from "next/error";
 
-type RequestType = {name: string};
-type ResponseType = Id<"workspaces"> | null;
+type RequestType = {name: string; workspaceId: Id<"workspaces">};
+type ResponseType = Id<"channels"> | null;
 
 type Options = {
     onSuccess?: (data: ResponseType) => void;
@@ -15,7 +15,7 @@ type Options = {
     throwError?: boolean;
 };
 
-export const useCreateWorkspace = () => {
+export const useCreateChannel = () => {
     const [data, setData] = useState<ResponseType>(null);
     const [error, setError] = useState<Error | null>(null);
 
@@ -31,7 +31,7 @@ export const useCreateWorkspace = () => {
     const isError = useMemo(() => status === 'error', [status]);
     const isSettled = useMemo(() => status === 'settled', [status]);
 
-    const mutation = useMutation(api.workspaces.create);
+    const mutation = useMutation(api.channels.create);
     
     const mutate = useCallback( async (values: RequestType, options?: Options) => {
         try {
