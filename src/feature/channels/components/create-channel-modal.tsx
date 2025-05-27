3f6f0efb4d6ed2c22f4f345eccc3a14d1dfd7c18
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-05-27 16:12:44 +1200

    adding channel function is up and running

--- src/feature/channels/components/create-channel-modal.tsx
+++ src/feature/channels/components/create-channel-modal.tsx
@@ -7,11 +7,17 @@ import {
     DialogTitle
 } from '@/components/ui/dialog';
 import { Input } from '@/components/ui/input';
-import { useState } from 'react';
+import { useWorkspaceId } from '@/hooks/use-workspace-id';
+import React, { useState } from 'react';
+import { useCreateChannel } from '../api/use-create-channel';
 
 import { useCreateChannelModal } from '../store/use-create-channel-modal';
 
 export const CreateChannelModal = () => {
+
+    const workspaceId = useWorkspaceId();
+
+    const { mutate, isPending } = useCreateChannel();
     
     const [open, setOpen] = useCreateChannelModal();
 
@@ -21,6 +27,20 @@ export const CreateChannelModal = () => {
         const value = e.target.value.replace(/\s+/g, "-").toLowerCase();
         setName(value);
     }
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        mutate({
+            name, workspaceId
+        }, {
+            onSuccess: (id) => {
+                //TODO redirect to new channel
+                handleClose();
+            }, 
+        }, 
+        
+        );
+    };
 
     const handleClose = () => {
         setName("");
@@ -35,10 +55,10 @@ export const CreateChannelModal = () => {
                         Add a channel
                     </DialogTitle>
                 </DialogHeader>
-                <form className="space-y-4">
+                <form onSubmit={handleSubmit} className="space-y-4">
                     <Input 
                         value={name}
-                        disabled={false}
+                        disabled={isPending}
                         onChange={handleChange}
                         required
                         autoFocus
