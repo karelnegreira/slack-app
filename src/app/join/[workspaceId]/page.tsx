9b8548af7316f26e689283aefe21b35308377a03
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-06-03 20:14:09 +1200

    mock logo is now visible in joincode page

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 
+import Image from "../../../../node_modules/next/image";
+
 const JoinPage = () => {
     return (
         <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-sm">
-            Join Page
+            <Image src="/next.svg" width={60} height="60" alt="Logo"/>
         </div>
     );
 }
