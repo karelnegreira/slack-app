d7e12b7a4831283d8b27c26a89cd6979282b4fe6
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-06-03 20:19:58 +1200

    adding some more syle to joincode page

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -6,6 +6,11 @@ const JoinPage = () => {
     return (
         <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-sm">
             <Image src="/next.svg" width={60} height="60" alt="Logo"/>
+            <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
+                <div className="flex flex-col gap-y-2 items-center justify-center">
+
+                </div>
+            </div>
         </div>
     );
 }
