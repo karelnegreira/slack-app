e8f606afb8f5ddefcd14345c7e6f210ed2587315
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-06-03 20:09:36 +1200

    adding some styling to joincode page

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -2,7 +2,7 @@
 
 const JoinPage = () => {
     return (
-        <div>
+        <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-sm">
             Join Page
         </div>
     );
