be55ac285d7840a1d473b6cfc53eec901f24ef3d
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-06-14 16:32:39 +1200

    dev channels  page adding styling and loaders

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,6 +1,35 @@
+"use client";
 
+import { Loader, TriangleAlert } from 'lucide-react'; 
+
+import { useGetChannel } from "@/feature/channels/api/use-get-channel";
+import { useChannelId } from "@/hooks/use-channel-id";
 
 const ChannelIdPage = () => {
+     
+     const channelId = useChannelId();
+
+     const {data: channel, isLoading: channelLoading} = useGetChannel({id: channelId});
+
+     if (channelLoading) {
+        return (
+            <div className="h-full flex-1 flex items-center justify-center">
+                <Loader className="animate-spin size-5 text-muted-foreground" />
+            </div>
+        );
+     }
+
+     if (channel) {
+        return (
+            <div className="h-full flex-1 flex flex-col gap-y-2 items-center justify-center">
+                <TriangleAlert className="size-6 text-muted-foreground" />
+                    <span className="text-sm text-muted-foreground">
+                        Channel not found
+                    </span>
+            </div>
+        );
+     }
+
   return (
     <div>
       Channel Id page
