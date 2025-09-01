c57e35ea15941fae9490fb589fe89c83b06edf5b
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-09-02 07:30:08 +1200

    update function added in channels.ts schema

--- convex/channels.ts
+++ convex/channels.ts
@@ -4,6 +4,39 @@ import {v} from 'convex/values';
 import { mutation, query } from "./_generated/server";
 import { auth } from "./auth";
 
+export const update = mutation({
+    args: { id: v.id("channel"),
+    workspaceId: v.id("workspaces"), 
+    name: v.string()
+}, 
+    handler: async (ctx, args) => {
+        const userId = await auth.getUserId(ctx);
+
+        if (!userId) {
+            return [];
+        }
+
+        const member = await ctx
+            .db
+            .query("members")
+            .withIndex("by_workspace_id_user_id",
+            (q: any) => q.eq("workspaceId", args.workspaceId)
+            .eq("userId", userId)
+        )
+            .unique();
+
+        if (!member || member.role !== 'admin') {
+            throw new Error("Unauthorized");
+        }
+
+        await ctx.db.patch(args.id, {
+            name: args.name, 
+        });
+
+        return args.id;
+    }, 
+});
+
 
 export const create = mutation({
     args: {
