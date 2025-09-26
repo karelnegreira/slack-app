39933ae281dbdf88a8f9e4a13df3da435ca5e51f
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-09-26 19:42:46 +1200

    quill package added in project aimed to editor styling

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,10 +1,17 @@
 
 
+import Quill from 'quill';
+
+import "quill/dist/quill.snow.css"; 
+import { useRef } from 'react';
+
 const Editor = () => {
+    const containerRef = useRef();
+    
   return (
     <div className="flex flex-col">
       <div className="flex flex-col border border-slate-300 rounded-md overflow-hidden focus-within:border-slate-400 focus-within:shadow-sm transition bg-white">
-
+        <div ref={containerRef}/>
       </div>
     </div>
   )
