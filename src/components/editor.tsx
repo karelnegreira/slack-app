cc82c31a768fad1a0bc90be5886d2226f2aea108
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-09-27 01:20:57 +1200

    Hint now says the function of each icon

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -7,6 +7,7 @@ import {PiTextAa} from 'react-icons/pi';
 import {Smile, ImageIcon} from 'lucide-react';
 import {MdSend} from 'react-icons/md';
 import { useEffect, useRef } from 'react';
+import { Hint } from './hint';
 
 import { Button } from './ui/button';
 
@@ -39,34 +40,44 @@ const Editor = () => {
       <div className="flex flex-col border border-slate-300 rounded-md overflow-hidden focus-within:border-slate-400 focus-within:shadow-sm transition bg-white">
         <div ref={containerRef} className="h-full ql-custom"/>
         <div className="flex px-2 pb-2 z-[5]">
-            <Button
+            <Hint label="Hide formatting">
+                <Button
+                    disabled={false}
+                    size="iconSm"
+                    variant="ghost"
+                    onClick={() => {} }
+                >
+                    <PiTextAa className="size-4"/>
+                    
+                </Button>
+            </Hint>
+            <Hint label="Emoji">
+                <Button
+                    disabled={false}
+                    size="iconSm"
+                    variant="ghost"
+                    onClick={() => {} }
+                >
+                    <Smile className="size-4"/>
+                    
+                </Button>
+            </Hint>
+            <Hint label="Image">
+                <Button
+                    disabled={false}
+                    size="iconSm"
+                    variant="ghost"
+                    onClick={() => {} }
+                >
+                    <ImageIcon className="size-4"/>
+                    
+                </Button>
+            </Hint>
+            <Button 
                 disabled={false}
+                onClick={() => {}}
                 size="iconSm"
-                variant="ghost"
-                onClick={() => {} }
-            >
-                <PiTextAa className="size-4"/>
-                
-            </Button>
-            <Button
-                disabled={false}
-                size="iconSm"
-                variant="ghost"
-                onClick={() => {} }
-            >
-                <Smile className="size-4"/>
-                
-            </Button>
-            <Button
-                disabled={false}
-                size="iconSm"
-                variant="ghost"
-                onClick={() => {} }
-            >
-                <ImageIcon className="size-4"/>
-                
-            </Button>
-            <Button className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white">
+                className="ml-auto bg-[#007a5a] hover:bg-[#007a5a]/80 text-white">
                 <MdSend className="size-4" />
             </Button>
         </div>
