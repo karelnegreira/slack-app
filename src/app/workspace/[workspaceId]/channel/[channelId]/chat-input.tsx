b884686eaa2bf8eb170f134aa175ab00534f5f76
Karel Negreira Fuentes <karelnegreira@gmail.com>
2025-09-27 20:46:15 +1200

    passing props to editor component from inputChat

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,13 +1,22 @@
 import dynamic from 'next/dynamic';
-
+import Quill from 'quill';
+import { useRef } from 'react';
 
 
 const Editor = dynamic(() => import("@/components/editor"), {ssr: false});
 
 const ChatInput = () => {
+
+    const editorRef = useRef<Quill | null>(null)
+
   return (
     <div className="px-5 w-full">
-      <Editor variant="update"/> 
+      <Editor 
+        placeholder="test your editor"
+        onSubmit={() => {}}
+        disabled={false}
+        innerRef={editorRef}
+      /> 
     </div>
   )
 }
